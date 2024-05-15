f33e0f84fde23bd1ba95117076aa7f4886d892b7
Djaner Salibryam <dfthae2019@proton.me>
2024-05-15 12:49:41 +0300

    Update structure of directories

--- Python-Fundamentals/Basic Syntax, Conditional Statements and Loops/maximum_multiple.py
+++
@@ -1,9 +0,0 @@
-def find_largest_integer(divisor, bound):
-    return bound // divisor * divisor
-
-
-divisor = int(input())
-bound = int(input())
-
-result = find_largest_integer(divisor, bound)
-print(result)
